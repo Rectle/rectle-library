2d56c1f5519aba57e31fd27e828979b5df8d5f57
Michał Bilski <michubilu@wp.pl>
2023-10-12 19:59:40 +0200

    Add send result logic

--- src/core/functions/send_result.py
+++ src/core/functions/send_result.py
@@ -1,18 +1,12 @@
-def send_result(*args, **kargs):
-    _args = _prepare_arguments(args, kargs)
+import socketio
+import os
+
+
+def send_result(score):
+    socket = socketio.Client()
+    headers = {"X-Build": os.environ["BUILD_ID"], "X-Token": os.environ["SECRET"]}
+    socket.connect(os.environ["SERVER_URL"], headers=headers, namespaces=["/private"])
+
+    socket.emit("build:result", score, namespace="/private")
+
     return True
-
-def _prepare_arguments(*args, **kargs):
-    _args = {}
-    
-    for key, value in kargs.items():
-        _args[key] = value
-
-    i = 0
-    for value in args:
-        while f"arg{i}" in _args:
-            i += 1
-
-        _args[f"arg{i}"] = value
-
-    return _args
